e2c391ca299192c19f52079cfcc7a9480faea080
PinoCoders <91031189+PinoCoders@users.noreply.github.com>
2021-10-01 01:58:08 +0700

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api-recodeteam.herokuapp.com';
+var url = 'https://zahirr-web.herokuapp.com';
 try {
-if(url == 'https://api-recodeteam.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://zahirr-web.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, NizamGanss','green'))
+  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
